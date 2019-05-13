9e58981ff70debf0ba3d9e4f636e7379f3df6f5e
etigui <32940684+etigui@users.noreply.github.com>
2019-05-13 19:42:55 +0200

    fill readme

--- reflector.py
+++ reflector.py
@@ -6,22 +6,6 @@ __version__ = "1.1.2"
 __maintainer__ = "Etienne Guignard"
 __email__ = "guignard.etienne@gmail.com"
 __status__ = "Production"
-# https://stackoverflow.com/questions/6778288/lat-lon-to-utm-to-lat-lon-is-extremely-flawed-how-come
-# https://fr.wikipedia.org/wiki/Transverse_universelle_de_Mercator
-# https://forums.tigsource.com/index.php?topic=34039.0
-# https://stackoverflow.com/questions/16266809/convert-from-latitude-longitude-to-x-y
-# https://www.pgc.umn.edu/apps/convert/
-
-## simplekml lib
-# https://simplekml.readthedocs.io/en/latest/
-
-## pyinstaller _datadir error
-# https://stackoverflow.com/questions/55824830/i-get-error-no-module-named-pyproj-datadir-after-i-made-py-to-exe-with-py
-
-## Make py to exe
-# pip install pyinstaller
-# pyinstaller reflect.py
-# GOTO dist directory and run reflect.exe
 
 import sys
 import os
